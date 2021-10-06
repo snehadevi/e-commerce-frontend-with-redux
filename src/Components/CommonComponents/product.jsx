4a724c2c40fcd5ca8ccf1fabae3d32b40204d781
snehadevi <snehadevi2016105@gmail.com>
2021-10-06 16:18:54 +0600

    image clickable

--- src/Components/CommonComponents/product.jsx
+++ src/Components/CommonComponents/product.jsx
@@ -10,7 +10,7 @@ import {
   ShoppingCartOutline,
 } from "heroicons-react";
 
-function Product({ product }) {
+function Product({ product, history }) {
   const dispatch = useDispatch();
   const { counts } = useSelector((state) => state);
 
@@ -18,28 +18,32 @@ function Product({ product }) {
 
   return (
     <div className="shadow-xl p-3 sm:w-80">
-      <div className="group relative">
-        <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-          <img
-            src={product.image}
-            alt={product.category}
-            className="w-full h-full object-center object-container sm:h-80 sm:w-80 lg:w-full lg:h-full"
-          />
-        </div>
-        <div className="h-60 sm:h-32 mt-4 flex justify-between">
-          <div className="justify-between mr-5">
-            <h3 className="text-sm text-gray-700">
-              <a>
-                <span aria-hidden="true" className="absolute inset-0" />
-                {product.title}
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+      <Link to={`/${product.id}`}>
+        <div className="group relative p-2">
+          <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+            <img
+              src={product.image}
+              alt={product.category}
+              className="w-full h-full object-center object-container sm:h-80 sm:w-80 lg:w-full lg:h-full"
+            />
           </div>
-          <p className="text-sm font-medium text-gray-900">${product.price}</p>
+          <div className="h-60 sm:h-32 mt-4 flex justify-between">
+            <div className="justify-between mr-5">
+              <h3 className="text-sm text-gray-700">
+                <a>
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {product.title}
+                </a>
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+            </div>
+            <p className="text-sm font-medium text-gray-900">
+              ${product.price}
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="justify-between">
+      </Link>
+      <div className="justify-between mt-2">
         {!count > 0 && (
           <div className="justify-between xl:flex flex-row ">
             <a

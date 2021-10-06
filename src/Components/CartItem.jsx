6c6adc20cedd5062f5b36c3b191be9af8eb29b37
snehadevi <snehadevi2016105@gmail.com>
2021-10-06 18:40:03 +0600

    cartitem button

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { actions } from "../Redux/Slice";
+import { MinusCircle, PlusCircle } from "heroicons-react";
 
 function CartItem({ product, total }) {
   const dispatch = useDispatch();
@@ -34,14 +35,32 @@ function CartItem({ product, total }) {
                                   </p> */}
         </div>
         <div className="flex-1 flex items-end justify-between text-lg">
-          <div className="text-gray-500 border-4 border-purple-600">
-            {/* Quantity{" "} */}
-            <input
-              required
-              value={counts[product.id]}
-              type="number"
-              className="w-8 h-8 focus:outline-none focus:ring focus:border-blue-300 opacity-100"
-              onChange={(e) => OnChange(e.target.value)}
+          <div className="flex flex-row">
+            <MinusCircle
+              onClick={(e) => {
+                if (counts[product.id] === 1) return;
+                dispatch(actions.Decrement({ product }));
+              }}
+              className={
+                counts[product.id] === 1
+                  ? "text-purple-700 h-8 w-8 mr-2"
+                  : "text-purple-700 h-8 w-8 mr-2 hover:text-purple-500"
+              }
+              aria-disabled={counts[product.id] === 1}
+            />
+            <div className="text-gray-500 border-4 border-purple-600">
+              {/* Quantity{" "} */}
+              <input
+                required
+                value={counts[product.id]}
+                type="number"
+                className="w-8 h-8 focus:outline-none focus:ring focus:border-blue-300 opacity-100"
+                onChange={(e) => OnChange(e.target.value)}
+              />
+            </div>
+            <PlusCircle
+              onClick={() => dispatch(actions.Increment({ product }))}
+              className="text-purple-700 h-8 w-8 ml-2 hover:text-purple-500"
             />
           </div>
 
